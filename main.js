ff4d8a9c541140effdc7640a2c53425103e5cbef
fgjer <priyankan27072002@gmail.com>
2023-12-18 15:27:58 -0800

    addnew

---
+++ main.js
@@ -0,0 +1,9 @@
+var n=prompt("Enter the number")
+for(i=1;i<=n;i++){
+    for(j=1;j<=i;j++){
+        document.write("&nbsp")
+        
+        document.write("*")
+    }
+    document.write("<br>")
+}
